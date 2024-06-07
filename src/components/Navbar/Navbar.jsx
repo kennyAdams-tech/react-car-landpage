ec3e733bfa127d9937685abeff84002b68402836
Adams kenny <kehindeadams72@gmail.com>
2024-06-06 22:13:24 -1100

    modified

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,23 @@
 import React, { useEffect, useState } from 'react'
+import { IoMdArrowDropdown } from 'react-icons/io'
 import './Navbar.css'
 
 const Navbar = () => {
   const [active, setActive] = useState('list')
   const [toggleIcon, setToggleIcon] = useState('hamburger')
   const [sticky, setSticky] = useState(true)
+  const [activeDropDwIcon, setActiveDropDwIcon] = useState('dropdownIcon')
+  const [dropdown, setDropdown] = useState('drop-down')
 
   const toggle = () => {
     active === 'list' ? setActive('list active') : setActive('list')
     toggleIcon === 'hamburger' ? setToggleIcon('hamburger active') : setToggleIcon('hamburger')
+  }
+
+  const dropDownToggle = () => {
+      dropdown === 'drop-down' ? setDropdown('drop-down show') : setDropdown('drop-down')
+
+      activeDropDwIcon === 'dropdownIcon' ? setActiveDropDwIcon('dropdownIcon rotate') : setActiveDropDwIcon('dropdownIcon')
   }
 
   useEffect(()=> {
@@ -23,7 +32,13 @@ const Navbar = () => {
       <nav className={`${sticky ? 'darknav' : ''}`}>
             <div className="logo">Logo</div>
             <ul className={active}>
-                <li>Home</li>
+                  <li className='home'>Home <IoMdArrowDropdown onClick={dropDownToggle} className={activeDropDwIcon}/> <ul className={dropdown}>
+                    <li>Dropdown 1</li>
+                    <li>Dropdown 2</li>
+                    <li>Dropdown 3</li>
+                    <li>Dropdown 4</li>
+                    </ul>
+                </li>
                 <li>About</li>
                 <li className='contact'>Contact</li>
             </ul>
